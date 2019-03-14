da3452d9859c64724621efad303c1e236437a118
wangym <wangyuming11725@gmail.com>
2019-03-14 11:06:14 +0800

    add unit test

--- src/test/java/com/wangym/gitlab/GitLabApiTest.java
+++ src/test/java/com/wangym/gitlab/GitLabApiTest.java
@@ -1,10 +1,12 @@
 package com.wangym.gitlab;
 
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.RepositoryApi;
 import org.gitlab4j.api.models.Branch;
+import org.gitlab4j.api.models.CompareResults;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class GitLabApiTest {
 
     @Autowired
@@ -30,6 +33,21 @@ public class GitLabApiTest {
                 .collect(Collectors.toList());
         for (Branch branch : branchs) {
             System.out.println(JSON.toJSONString(branch));
+        }
+    }
+
+    @Test
+    public void compareTest() throws GitLabApiException {
+        RepositoryApi repositoryApi = gitLabApi.getRepositoryApi();
+        List<Branch> branchs = repositoryApi.getBranches(739).stream()
+                // 待比较的分支排除master
+                .filter(it -> !it.getName().equals("master"))
+                .collect(Collectors.toList());
+        for (Branch branch : branchs) {
+            CompareResults result = repositoryApi.compare(739, "master", branch.getName());
+            if (result.getDiffs().isEmpty()) {
+                log.info("当前分支的全部变更已在master中存在，此分支可以去除,branch：{},{}", branch.getName());
+            }
         }
     }
 
