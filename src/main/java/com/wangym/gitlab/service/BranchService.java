0982b2b3533d3b10c1ff1d35ecc4f9096eaf9f11
wangym <wangyuming11725@gmail.com>
2019-03-15 10:44:13 +0800

    如果非master分支与master分支进行比较，如果不存在diff说明此分支是可以删除的

--- src/main/java/com/wangym/gitlab/service/BranchService.java
+++ src/main/java/com/wangym/gitlab/service/BranchService.java
@@ -7,6 +7,7 @@ import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.GroupApi;
 import org.gitlab4j.api.RepositoryApi;
 import org.gitlab4j.api.models.Branch;
+import org.gitlab4j.api.models.CompareResults;
 import org.gitlab4j.api.models.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -43,7 +44,8 @@ public class BranchService {
             for (Project p : projects) {
                 log.info("开始查询项目分支信息，group：{},project：{}", p.getNamespace().getFullPath(), p.getName());
                 Integer projectId = p.getId();
-                List<Branch> branchs = repositoryApi.getBranches(projectId).stream()
+                List<Branch> branches = repositoryApi.getBranches(projectId);
+                List<Branch> branchs = branches.stream()
                         // 已经合并入master的分支可以考虑去删除
                         .filter(Branch::getMerged)
                         // 过滤出不是想要保留的分支
@@ -52,8 +54,15 @@ public class BranchService {
                 for (Branch branch : branchs) {
                     String branchName = branch.getName();
                     log.info("[当前分支已合并进入master]:{},{},{}", p.getName(), branchName, branch.getMerged());
-                    // repositoryApi.deleteBranch(projectId, branchName);
+                    deleteBranch(projectId, branchName);
                 }
+                List<Branch> branchs2 = branches.stream()
+                        // 已经合并入master的分支可以考虑去删除
+                        .filter(it -> !it.getMerged())
+                        // 过滤出不是想要保留的分支
+                        .filter(it -> !protectBranch.contains(it.getName()))
+                        .collect(Collectors.toList());
+                compareTest(branchs2, projectId);
 
             }
         } catch (GitLabApiException e) {
@@ -68,6 +77,27 @@ public class BranchService {
             return groupApi.getProjects(groupId);
         } catch (GitLabApiException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void compareTest(List<Branch> branchs, Integer projectId) throws GitLabApiException {
+        RepositoryApi repositoryApi = gitLabApi.getRepositoryApi();
+        for (Branch branch : branchs) {
+            String name = branch.getName();
+            CompareResults result = repositoryApi.compare(projectId, "master", name);
+            if (result.getDiffs().isEmpty()) {
+                log.info("[当前分支的全部变更已在master中存在，此分支可以去除],branch：{}", name);
+                deleteBranch(projectId, name);
+            }
+        }
+    }
+
+    private void deleteBranch(Integer projectId, String branchName) {
+        RepositoryApi repositoryApi = gitLabApi.getRepositoryApi();
+        try {
+            repositoryApi.deleteBranch(projectId, branchName);
+        } catch (GitLabApiException e) {
+            log.error("删除分支失败，分支名:{}", branchName, e);
         }
     }
 }
